246bea2744b49d1b9aec827c85eaec12287486b0
wesselpawel <wesselpawel@users.noreply.github.com>
2022-04-29 14:56:00 +0200

    heroku

--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ app.use("/user", userRouter);
 
 //mongodb
 const CONNECTION_URL = 'mongodb+srv://wesselpawel:12345QWERTY@cluster0.0hsed.mongodb.net/paznokietki-mern';
-const PORT = 'https://paznokietki-mern.herokuapp.com/';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server is running`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
