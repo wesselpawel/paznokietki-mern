494f391059e9d21bf5217d909066716f414e13e5
wesselpawel <wesselpawel@users.noreply.github.com>
2022-04-29 15:26:33 +0200

    heroku

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+const path = require('path');
 
 import announcementRoutes from './routes/announcements.js';
 import userRouter from "./routes/user.js";
@@ -15,6 +16,13 @@ app.use(cors());
 app.use('/announcements', announcementRoutes)
 app.use("/user", userRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('frontend/build'))
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 //mongodb
