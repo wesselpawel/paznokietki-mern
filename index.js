0d4aed4a5cabe04aa4c2b9b1f0f1fa6716a1b065
wesselpawel <wesselpawel@users.noreply.github.com>
2022-04-29 15:34:54 +0200

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require (express);
+const bodyParser = require (body-parser);
+const mongoose = require (mongoose);
+const cors = require (cors);
 const path = require('path');
 
 import announcementRoutes from './routes/announcements.js';
